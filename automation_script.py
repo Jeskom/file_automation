1962a34f0188766f2b6ccdfe9789ef049768bace
Jonathan <67198886+Jeskom@users.noreply.github.com>
2024-09-21 09:56:15 +0400

    Add files via upload

--- automation_script.py
+++ automation_script.py
@@ -1,26 +1,30 @@
 import os
 import shutil
+import time
 
-#Change directories according to your operating system and username
-SOURCE_DIR = "/Users/[username]/Downloads"
-DOCUMENTS_DIR = "/Users/[username]/Documents"
-PICTURES_DIR = "/Users/[username]/Pictures"
-MOVIES_DIR = "/Users/[username]/Movies"
+SOURCE_DIR = "/Users/jonok/Downloads"
+DOCUMENTS_DIR = "/Users/jonok/Documents"
+PICTURES_DIR = "/Users/jonok/Pictures"
+MOVIES_DIR = "/Users/jonok/Movies"
 
 document_index = '.doc', '.docx', '.pdf', '.txt', '.odt', '.rtf', '.xls', '.xlsx', '.ppt', '.pptx', '.csv', '.ods', '.odp', '.md', '.epub', '.pages'
 picture_index = '.jpeg', '.jpg', '.png', '.gif', '.bmp', '.tiff', '.tif', '.svg', '.heif', '.heic', '.webp', '.raw', '.cr2', '.nef', '.arw', '.orf', '.dng', '.rw2'
 movies_index = '.mp4', '.mov', '.wmv', '.avi', '.mkv', '.flv', '.mpeg', '.mpg', '.m4v', '.webm', '.3gp'
 
+#Gets the files from the directory
 def sorted_directory(directory):
     items = os.listdir(directory)
     sorted_items = sorted(items)
     return sorted_items
 
-sorted_list = sorted_directory(SOURCE_DIR)
-sorted_list = sorted_list[2:]
+#Sorts the files and hides the 2 system files
+def sorting():
+    sorted_list = sorted_directory(SOURCE_DIR)
+    sorted_list = sorted_list[2:]
+    return sorted_list
 
+#Moves the files
 def file_mover(file):
-
     for fname in file:
         if fname.lower().endswith(document_index):
             shutil.move(os.path.join(SOURCE_DIR, fname), DOCUMENTS_DIR)
@@ -31,7 +35,12 @@ def file_mover(file):
         elif fname.lower().endswith(movies_index):
             shutil.move(os.path.join(SOURCE_DIR, fname), MOVIES_DIR)
             print(f"{fname} has been moved to {MOVIES_DIR}")
-
     return True
 
-file_mover(sorted_list)
+#Runs the program
+def main():
+    while True:
+        file_mover(sorting())
+        time.sleep(600)
+
+main()
