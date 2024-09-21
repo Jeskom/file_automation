ccc032ffb13ba71a69f87dbe32d551a1d8cbe320
Jonathan <67198886+Jeskom@users.noreply.github.com>
2024-09-21 09:10:47 +0400

    Add files via upload

---
+++ automation_script.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+
+SOURCE_DIR = "/Users/jonok/Downloads"
+DOCUMENTS_DIR = "/Users/jonok/Documents"
+PICTURES_DIR = "/Users/jonok/Pictures"
+MOVIES_DIR = "/Users/jonok/Movies"
+
+document_index = '.doc', '.docx', '.pdf', '.txt', '.odt', '.rtf', '.xls', '.xlsx', '.ppt', '.pptx', '.csv', '.ods', '.odp', '.md', '.epub', '.pages'
+picture_index = '.jpeg', '.jpg', '.png', '.gif', '.bmp', '.tiff', '.tif', '.svg', '.heif', '.heic', '.webp', '.raw', '.cr2', '.nef', '.arw', '.orf', '.dng', '.rw2'
+movies_index = '.mp4', '.mov', '.wmv', '.avi', '.mkv', '.flv', '.mpeg', '.mpg', '.m4v', '.webm', '.3gp'
+
+def sorted_directory(directory):
+    items = os.listdir(directory)
+    sorted_items = sorted(items)
+    return sorted_items
+
+sorted_list = sorted_directory(SOURCE_DIR)
+sorted_list = sorted_list[2:]
+
+def file_mover(file):
+
+    for fname in file:
+        if fname.lower().endswith(document_index):
+            shutil.move(os.path.join(SOURCE_DIR, fname), DOCUMENTS_DIR)
+            print(f"{fname} has been moved to {DOCUMENTS_DIR}")
+        elif fname.lower().endswith(picture_index):
+            shutil.move(os.path.join(SOURCE_DIR, fname), PICTURES_DIR)
+            print(f"{fname} has been moved to {PICTURES_DIR}")
+        elif fname.lower().endswith(movies_index):
+            shutil.move(os.path.join(SOURCE_DIR, fname), MOVIES_DIR)
+            print(f"{fname} has been moved to {MOVIES_DIR}")
+
+    return True
+
+file_mover(sorted_list)
